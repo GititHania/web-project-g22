1859414c6995306f47ceb9935cd757d542227a54
GititHania <gitit.hania@gmail.com>
2021-12-08 18:49:49 +0200

    summary, registrtion, login, assamble pages

--- main.js
+++ main.js
@@ -7,7 +7,37 @@ function find_location() {
 }
 
 
-function listen_to_questions(){
+function close_form(form_id) {
+    document.getElementById("page-mask").style.display = "none";
+    var forms = document.getElementsByClassName('popup');
+    for (let i = 0; i < forms.length; i++) {
+        forms[i].style.display = "none";
+    }
+}
+
+function open_form(form_id) {
+    document.getElementById(form_id).style.display = "block";
+    document.getElementById("page-mask").style.display = "block";
+
+}
+
+function get_summery(){
+    var details = {
+        "StoreName": "תל אביב, דיזניגוף 205",
+        "StorePhone": "052-9856147",
+        "PaymentMethod":"אשראי",
+        "Cost":'90 ש"ח',
+        "ATA":"כ - 35 דקות",
+    }
+    getElementById("StoreName").innerHTML = details.StoreName;
+    getElementById("StorePhone").innerText = details.StorePhone;
+    getElementById("Cost").innerText = details.Cost;
+    getElementById("PaymentMethod").innerText = details.PaymentMethod;
+    getElementById("ATA").innerText = details.ATA;
+
+}
+
+function listen_to_questions() {
 
     var faq = document.getElementsByClassName("faq-question");
     var i;
